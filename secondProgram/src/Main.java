887fd3df8a2646d3a2ffc8aeac99a6c0980cc51e
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-03 01:12:18 +0600

    Adnan coder

---
+++ secondProgram/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+    public int id;
+    public String title;
+    public float price;
+
+    public void display() {
+        System.out.println(id);
+
+        System.out.println(title);
+
+        System.out.println(price);
+
+    }
+
+}
---
+++ secondProgram/src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    public static void main(String[] args) {
+
+
+        Main obj = new Main();
+        obj.id = 12;
+        obj.title = "beautiful";
+        obj.price = 12.3F;
+        obj.display();
+
+    }
+}
