f054840b84f8a8d1244e45bd827a4d9bd220c9e2
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-03 01:44:38 +0600

    Coded by Adnan

---
+++ Assignment-2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        product obj = new product();
+
+        obj.id= 21;
+        obj.category="large";
+        obj.price=323.32f;
+        obj.title="Beautiful ";
+        obj.description= "This is descreaption ";
+        obj.display();
+
+
+
+    }
+}
---
+++ Assignment-2/src/product.java
@@ -0,0 +1,23 @@
+public class product {
+
+int id ;
+String title ;
+float price ;
+
+String description ;
+ String category;
+
+
+    void display()
+    {
+        System.out.println( "This  is my id "+id );
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(description);
+        System.out.println(category);
+
+    }
+
+
+
+}
