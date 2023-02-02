1a2a9d02acab90131669a6fd0388126102ddccf1
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-02 23:56:24 +0600

    Adnan coder

---
+++ fastProgram/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student obj = new Student ();
+
+        obj.name = "Adnan";
+        obj.age =21;
+        obj.cgpa=23.3F;
+        obj.display();
+    }
+}
---
+++ fastProgram/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+
+    String name ;
+    int age ;
+
+    float cgpa ;
+
+    void display(){
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(cgpa);
+
+    }
+
+}
