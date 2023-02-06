479971ce96eba007899c8eb2c6bcaba83b8ced6f
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-06 20:21:51 +0600

    coded by Adnan

---
+++ Lab-3-p2/src/Main.java
@@ -0,0 +1,17 @@
+
+
+//2. Create main class, main method and print something
+
+public class Main {
+
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("My name is :Adnan ");
+        System.out.println("Student id :221 - 15-5743 ");
+        System.out.println("Home town  : Rangpur  ");
+
+    }
+
+}
