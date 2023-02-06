9026ce94111c431fd03bf8905b9280cdb3a7cb47
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-06 20:23:06 +0600

    coded by Adnan

---
+++ Lab-3-p3/src/Box.java
@@ -0,0 +1,14 @@
+
+//3. Create Box class (variable: height, width, length; method: calculate_area()) and its object
+public class Box {
+
+    public static void main(String[] args) {
+
+        Main obj1 = new Main();
+
+        obj1.height = 9;
+        obj1.weight = 7;
+        obj1.length = 5;
+        obj1.calculate_area();
+    }
+}
---
+++ Lab-3-p3/src/Main.java
@@ -0,0 +1,13 @@
+//3. Create Box class (variable: height, width, length; method: calculate_area()) and its object
+public class Main {
+    int height ;
+    int weight;
+    int length ;
+    void calculate_area() {
+
+        int area = height * weight * length;
+        System.out.println("The area is =" + area);
+
+
+    }
+}
