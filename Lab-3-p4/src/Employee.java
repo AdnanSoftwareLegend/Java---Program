4fd4adfee5ae05d6cf65b5de304af2442a2c3d28
Adnan Elahe <123932208+AdnanSoftwareLegend@users.noreply.github.com>
2023-02-06 20:24:04 +0600

    coded by Adnan

---
+++ Lab-3-p4/src/Employee.java
@@ -0,0 +1,16 @@
+
+
+//4. Create Employee class (variable: name, salary; method: display()) and its object
+public class Employee {
+
+    public static void main(String[] args) {
+
+        Main obj1 = new Main();
+
+        obj1.name = "Adnan Elahe";
+        obj1.salary = 54000;
+        obj1.display();
+
+
+    }
+}
---
+++ Lab-3-p4/src/Main.java
@@ -0,0 +1,17 @@
+
+//4. Create Employee class (variable: name, salary; method: display()) and its object
+public class Main {
+    String name ;
+    double salary;
+
+    void display()
+    {
+        System.out.println (name );
+        System.out.println (salary);
+
+    }
+
+
+
+
+}
